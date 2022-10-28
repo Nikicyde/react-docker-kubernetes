9b11214f61307fb948185ad090d388b5013e5942
nikicyde <niki.szabo6@gmail.com>
2022-10-28 13:55:14 +0200

    first commit

---
+++ react-docker/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>
+          + Hello from Docker image running on K3s :)
+        </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
