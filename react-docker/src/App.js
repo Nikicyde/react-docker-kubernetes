b75326479ce427321c1159139e8c9c829f0adad5
nikicyde <niki.szabo6@gmail.com>
2022-10-31 08:57:37 +0100

    test env

--- react-docker/src/App.js
+++ react-docker/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>
-          Hello World!
+          Hello Prod World!
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
