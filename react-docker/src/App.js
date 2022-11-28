1c9b321443d2883a9c7cceb59f0b8e48594d96f6
nikicyde <niki.szabo6@gmail.com>
2022-11-28 13:09:00 +0100

    world

--- react-docker/src/App.js
+++ react-docker/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1 id="hello">
-          Hello Krone!
+          Hello World!
         </h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
